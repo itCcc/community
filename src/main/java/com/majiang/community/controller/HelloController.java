8500030ace5ee534dd74d3b9dd70626cf3a48584
Ccc <c5153520@126.com>
2019-10-02 09:26:09 +0800

    init repo

---
+++ src/main/java/com/majiang/community/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author solang
+ * @date 2019-10-01 18:05
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
